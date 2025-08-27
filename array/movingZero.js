ed7190be9928ba22d56b1db168692fb4819b5396
Shubham Bisht <shubhambisht758@gmail.com>
2025-08-27 11:50:51 +0530

    add movingZero question

---
+++ array/movingZero.js
@@ -0,0 +1,18 @@
+//Move zero in place-x should contain the pos where we need have to shift non zero element
+//brute force approch-create anew array and fill number 
+arr=[0,1,0,3,12]
+let x=0
+for(let i=0;i<arr.length;i++){
+    if (arr[i]!==0){
+        arr[x]=arr[i];
+        x++;
+
+    }
+
+
+}
+//fill all the remaining element to zero
+for(let i=x;i<arr.length;i++){
+    arr[i]=0
+}
+console.log(arr)
